e0471227ca807435adf24bd6772cf1b87f81bcaa
shirinipaz <alishirinipazy@gmail.com>
2025-02-12 13:17:21 +0330

    the first commit and create by first ui

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <LayoutsHeader />
+  <nuxt-page/>
+  <LayoutsFooter/>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+        extend: {
+            aspectRatio: {
+                auto: 'auto',
+                square: '1 / 1',
+                video: '16 / 9'
+            }
+        }
+    }
+};
