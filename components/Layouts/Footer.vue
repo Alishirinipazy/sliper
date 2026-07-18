dd465d13ec8e2a784fda9da1227593cb20a25517
shirinipaz <alishirinipazy@gmail.com>
2026-07-18 15:21:52 +0330

    done end

--- components/Layouts/Footer.vue
+++ components/Layouts/Footer.vue
@@ -67,7 +67,7 @@
           <br>
           <a class="py-2" href="tel:+989372396496">09372396496</a>
           <br>
-          <a referrerpolicy='origin' target='_blank' href='https://trustseal.enamad.ir/?id=6942951&Code=I2pfln9Epa4AcXM0lSUb37egd1Fwq0Nb'><img referrerpolicy='origin' src='https://trustseal.enamad.ir/logo.aspx?id=6942951&Code=I2pfln9Epa4AcXM0lSUb37egd1Fwq0Nb' alt='' style='cursor:pointer' code='I2pfln9Epa4AcXM0lSUb37egd1Fwq0Nb'></a>
+          <a referrerpolicy='origin' target='_blank' href='https://trustseal.enamad.ir/?id=759742&Code=I2pfln9Epa4AcXM0lSUb37egd1Fwq0Nb'><img referrerpolicy='origin' src='https://trustseal.enamad.ir/logo.aspx?id=759742&Code=I2pfln9Epa4AcXM0lSUb37egd1Fwq0Nb' alt='' style='cursor:pointer' code='I2pfln9Epa4AcXM0lSUb37egd1Fwq0Nb'></a>
         </div>
 
       </div>
